c4e67a749831123bd1f788ae97c4cefcd3e094cc
Afton Gauntlett <afton.gauntlett@gmail.com>
2026-01-20 17:17:25 -0500

    Replace mock companies data with JSON dataset

--- src/data/types.ts
+++ src/data/types.ts
@@ -2,9 +2,7 @@ export type Company = {
   id: string;
   name: string;
   careersUrl?: string;
-
   locations: string[];
   employmentType: "Remote" | "Hybrid" | "In-office";
-
   interviewProcess?: string;
 };
